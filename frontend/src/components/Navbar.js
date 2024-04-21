b4500d8419b71aed659b624f081ca277f2b11a91
rohandarade <rohandarade29@gmail.com>
2024-04-22 04:34:41 +0530

    Changed title

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div className=''>
-        <div className='px-6 py-4'>PortfolioPro</div>
+        <div className='px-6 py-4 font-extrabold'>PortfolioPro</div>
         <hr className='border-t border-gray-300' />
     </div>
   )
